3920a2480eb96c4c8f54f73e373d2579e20feb26
Ray Maldonado <rm01964@rmn.com>
2020-05-27 16:57:22 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import data from './data/data.json';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('./public'));
+
+
+app.listen(PORT, () => {
+    console.log(`Your server is running on port ${PORT}`);
+    console.log(data);
+
+});
+
+app.get('/', (req, res) => {
+    // get data first
+    res.json(data);
+});
+
+app.post('/newItem', (req, res) => {
+    res.send(`a post request with /newItem route on port ${PORT}`);
+});
+
+app.put('/item', (req, res) => {
+    res.send(`a put request with /item route on port ${PORT}`);
+});
+
+app.delete('/item', (req, res) => {
+    res.send(`a delete request with /item route on port ${PORT}`);
+});
+
+
